bce6fe68343d73bb9f35263eeab6e9e9e6b8574a
Ethan Margaillan <ethan.margaillan@gmail.com>
2019-04-29 14:15:20 +0200

    Update Conda build

--- conda/build.sh
+++ conda/build.sh
@@ -1,37 +1,33 @@
 #!/bin/bash
 
-
 set -x -e
+
+export CXXFLAGS="-stdlib=libc++ -std=c++17"
+export LINKFLAGS="-stdlib=libc++ -std=c++17"
 
 if [ "$(uname)" == "Darwin" ];
 then
     export CC=clang
     export CXX=clang++
 
-    export MACOSX_VERSION_MIN=10.7
-    export CXXFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
-    export CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11"
-    export LINKFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
-    export LINKFLAGS="${LINKFLAGS} -stdlib=libc++ -std=c++11 "
-
+    export MACOSX_VERSION_MIN=10.11
     export QMAKESPEC=macx-g++
+	
+	CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
+    LINKFLAGS="${LINKFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
 
 fi
 
 if [ "$(uname)" == "Linux" ];
 then
-
     export QMAKESPEC=linux-g++
 fi
 
 [ -d build ]] || mkdir build
 cd build
 
-
 qmake PREFIX=$PREFIX NO_QT_VERSION_SUFFIX=1 ../libQGLViewer.pro
 
 make
 make install
 cd ..
-
-
